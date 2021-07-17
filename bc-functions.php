7b59214b366d8b48fcd39451c5fc103eb16afcf2
beavercoffee <beavercoffee@vidsoe.com>
2021-07-17 14:08:22 -0600

    BC

--- bc-functions.php
+++ bc-functions.php
@@ -12,7 +12,7 @@ Plugin URI: https://github.com/beavercoffee/bc-functions
 Requires at least: 5.7
 Requires PHP: 5.6
 Text Domain: bc-functions
-Version: 1.7.17
+Version: 1.7.17.1
 */
 
 if(defined('ABSPATH')){
